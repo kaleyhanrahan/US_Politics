b348bc6563e2b001e3c401ab3178196a2bc4a884
Kaley <kaley.hanrahan@gmail.com>
2017-04-08 14:15:41 -0400

    Update presDebates-featureEngineering.py

--- presDebates-featureEngineering.py
+++ presDebates-featureEngineering.py
@@ -1,3 +1,7 @@
+# For this project, I downloaded the full transcripts of each of the 2016 US Presidential Debates and
+# perform data cleaning and feature engineering. 
+# https://www.kaggle.com/mrisdal/2016-us-presidential-debates
+
 import numpy as np
 import pandas as pd
 import os
